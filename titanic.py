f8032e3ed5536f54a9f6bb9567c9a8947159f821
Mohit Gaur <56885276+Mohit-Gaur@users.noreply.github.com>
2020-04-10 13:03:30 +0530

    Update titanic.py
    Created a Logistic Regression Model to predict which passengers survived the sinking of Titanic based on features like age, class etc.

--- titanic.py
+++ titanic.py
@@ -11,17 +11,21 @@ passengers = pd.read_csv('passengers.csv')
 
 # Update sex column to numerical
 passengers['Sex'] = passengers['Sex'].map({'male': 0, 'female': 1})
+
 # Fill the nan values in the age column
 passengers['Age'].fillna(inplace = True, value = round(passengers['Age'].mean()))
+
 # Create a first class column
 passengers['FirstClass'] = passengers['Pclass'].apply(lambda p: 1 if p == 1 else 0)
 
 # Create a second class column
 passengers['SecondClass'] = passengers['Pclass'].apply(lambda p: 1 if p == 2 else 0)
 print(passengers)
+
 # Select the desired features
 features = passengers[['Sex', 'Age', 'FirstClass', 'SecondClass']]
 survival = passengers['Survived']
+
 # Perform train, test, split
 train_features, test_features, train_labels, test_labels = train_test_split(features,survival)
 
@@ -29,9 +33,11 @@ train_features, test_features, train_labels, test_labels = train_test_split(feat
 scaler = StandardScaler()
 train_features = scaler.fit_transform(train_features)
 test_features = scaler.transform(test_features)
+
 # Create and train the model
 model = LogisticRegression()
 model.fit(train_features, train_labels)
+
 # Score the model on the train data
 print(model.score(train_features, train_labels))
 
@@ -44,7 +50,7 @@ print(model.coef_)
 # Sample passenger features
 Jack = np.array([0.0,20.0,0.0,0.0])
 Rose = np.array([1.0,17.0,1.0,0.0])
-You = np.array([0.0,24,0,0.0,1.0])
+You = np.array([0.0,24.0,0.0,1.0])
 
 # Combine passenger arrays
 sample_passengers = np.array([Jack, Rose, You])
@@ -53,4 +59,5 @@ sample_passengers = np.array([Jack, Rose, You])
 sample_passengers = scaler.transform(sample_passengers)
 
 # Make survival predictions!
-
+print(model.predict(sample_passengers))
+print(model.predict_proba(sample_passengers))
